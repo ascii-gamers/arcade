b675a57bc2563944722620609dad5f780e4a9e48
Jack Cook <hello@jackcook.com>
2022-05-01 14:40:20 -0400

    Fix build

---
+++ arcade/leave.go
@@ -0,0 +1,21 @@
+package arcade
+
+import (
+	"encoding/json"
+)
+
+type LeaveMessage struct {
+	Message
+	Player *Player
+}
+
+func NewLeaveMessage(player *Player) *LeaveMessage {
+	return &LeaveMessage{
+		Message: Message{Type: "leave"},
+		Player:  player,
+	}
+}
+
+func (m LeaveMessage) MarshalBinary() ([]byte, error) {
+	return json.Marshal(m)
+}
